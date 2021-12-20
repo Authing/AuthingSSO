026fd1fa98da260ca93f4a8ac5c8b98600528106
zhaoyiming0803 <zhaoyiming0803@gmail.com>
2021-12-20 17:38:18 +0800

    chore: update jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  testMatch: [
+    '**/src/**/*.test.ts'
+  ]
+};
